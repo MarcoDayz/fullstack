c93990b246c8d4e295bab6cce9f5b6028a74e429
MarcoDayz <diaz.marco8686@gmail.com>
2022-09-02 15:37:24 -0700

    updated express api

--- db/goaltable.sql
+++ db/goaltable.sql
@@ -1,3 +1,11 @@
 DROP TABLE goals;
 
-CREATE TABLE goals()
+CREATE TABLE goals(
+    id serial,
+    first_name text,
+    last_name text,
+    goal_descr text,
+    complete boolean
+);
+
+INSERT INTO goals(first_name,last_name,goal_descr,complete) VALUES('marco','diaz','Graduate MCSP-14', 'true');
--- expressServer.js
+++ expressServer.js
@@ -1,60 +1,134 @@
 import express from "express";
 const app = express();
-// import pkg from "pg";
-// const {Pool} = pkg;
+import pkg from "pg";
+const {Pool} = pkg;
 
 const port = 4000;
 
-// const pool = new Pool({
-//     user:'markdays',
-//     password:'',
-//     host:'localhost',
-//     port: 5432,
-//     database:'goals'
-// });
+const pool = new Pool({
+    user:'markdays',
+    password:'',
+    host:'localhost',
+    port: 5432,
+    database:'goals_db'
+});
 
-//read all
-app.get('/', async (req,res)=>{
+// app.use(express.static('public'));
+
+app.use(express.json())
+
+//read all goals
+app.get('/goals', async (req,res)=>{
     try {
-        res.send('server is connected')
+        const { rows } = await pool.query(`SELECT * FROM goals`);
+            res.status(200);
+            res.contentType('application/json');
+            res.send(rows);
     } catch (error) {
-        console.error(error.message);
+            res.status(404);
+            res.contentType('text/plain');
+            res.send('page not found');
     }
 });
 
-//read one
-app.get('/:id', async (req,res)=>{
+//read one goal
+app.get('/goals/:id', async (req,res)=>{
     try {
-        res.send('server is connected')
+  
+        const { id } = req.params;
+        const { rows } = await pool.query(`SELECT * FROM goals WHERE id = $1`,[id]);
+        if(rows.length === 0){
+            res.status(404);
+            res.contentType('text/plain');
+            res.send('page not found');
+        }else{
+            res.status(200);
+            res.contentType('application/json');
+            res.send(rows);
+        }
     } catch (error) {
-        console.error(error.message);
+            res.status(404);
+            res.contentType('text/plain');
+            res.send('page not found');
     }
 });
 
-//create
-app.post('/', async (req,res)=>{
+//create new goal
+app.post('/goals', async (req,res)=>{
     try {
-        res.send('server is connected')
+        const { first_name, last_name,goal_descr,complete } = req.body;
+        
+        if(first_name && last_name && goal_descr && complete){
+            await pool.query(`INSERT INTO goals(first_name,last_name,goal_descr,complete) VALUES($1,$2,$3,$4)`,[first_name,last_name,goal_descr,complete]);
+        const {rows} = await pool.query(`SELECT * FROM goals`);
+            res.status(200);
+            res.contentType('application/json');
+            res.send(rows);
+        }else{
+            res.status(404);
+            res.contentType('text/plain');
+            res.send('page not found');
+        }
     } catch (error) {
-        console.error(error.message);
+            res.status(404);
+            res.contentType('text/plain');
+            res.send('page not found');
     }
 });
 
-//update 1
-app.put('/:id', async (req,res)=>{
+//update 1 goal
+app.put('/goals/:id', async (req,res)=>{
     try {
-        res.send('server is connected')
+        const { id } = req.params;
+        const { first_name,last_name,goal_descr,complete } = req.body;
+
+        if(rows.leng === 0){
+            res.status(404);
+                res.contentType('text/plain');
+                res.send('page not found');
+        }else{
+        const { rows } = await pool.query(`UPDATE goals SET first_name = $1, last_name = $2, goal_descr = $3, complete = $4 WHERE id = $5`, [first_name,last_name,goal_descr,complete,id]);
+            res.status(200);
+            res.contentType('application/json');
+            res.send(rows);
+        }
     } catch (error) {
-        console.error(error.message);
+            res.status(404);
+            res.contentType('text/plain');
+            res.send('page not found');
     }
+
+
 });
 
 //get delete 1
-app.delete('/:id', async (req,res)=>{
+app.delete('/goals/:id', async (req,res)=>{
     try {
-        res.send('server is connected')
+        const { id } = req.params;
+         await pool.query(`DELETE FROM goals WHERE id = $1`,[id]);
+        const {rows} = await pool.query(`SELECT * ALL goals`);
+            res.status(200);
+            res.contentType('application/json');
+            res.send(rows);
     } catch (error) {
-        console.error(error.message);
+            res.status(404);
+            res.contentType('text/plain');
+            res.send('page not found');
+    }
+});
+
+//delete all
+app.delete('/goals', async (req,res)=>{
+    try{
+        await pool.query(`DELETE FROM goals`);
+        // const {rows} = await pool.query(`select * from users`);
+            res.status(200);
+            res.contentType('application/json');
+            res.send('Table has been deleted');
+    }catch(error){
+            res.status(404);
+            res.contentType('text/plain');
+            res.send('page not found');
     }
 });
 
